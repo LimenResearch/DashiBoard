8937ae56f1994762a6043a909736cf4d49333cc6
piever <pietro.vertechi@protonmail.com>
2024-10-04 16:48:07 +0200

    wip

--- pipelines-react/src/App.jsx
+++ pipelines-react/src/App.jsx
@@ -2,9 +2,9 @@ import {useState} from "react"
 import {PathPicker} from "./components/path-picker";
 
 export function App() {
-    const [state, setState] = useState([[], null]);
-    console.log(state);
+    const [paths, setPaths] = useState([]);
+    console.log(paths);
     return <PathPicker permissionMessage="Enable folder"
-        fileMessage="Load files" state={state} setState={setState}>
+        fileMessage="Load files" setState={setPaths}>
     </PathPicker>;
 }

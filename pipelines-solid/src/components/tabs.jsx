70b61f61caedf390afaceecbc6cc6d3d4194e605
piever <pietro.vertechi@protonmail.com>
2024-11-13 16:56:32 +0100

    highlight current tab

--- pipelines-solid/src/components/tabs.jsx
+++ pipelines-solid/src/components/tabs.jsx
@@ -1,15 +1,18 @@
 import { createSignal } from "solid-js";
 
-function classList(submit) {
+function classList(active, submit) {
     return {
         "rounded"              : true,
+        "cursor-pointer"       : true,
         "py-2"                 : true,
         "px-4"                 : true,
         "text-2xl"             : true,
         "font-semibold"        : true,
-        "text-blue-800"        : true,
-        "hover:text-blue-900"  : true,
-        "hover:bg-gray-200"    : !submit,
+        "text-blue-800"        : !active,
+        "hover:text-blue-900"  : !active,
+        "text-blue-900"        : active,
+        "bg-gray-200"          : active,
+        "hover:bg-gray-200"    : !submit && !active,
         "bg-blue-100"          : submit,
         "hover:bg-blue-200"    : submit,
         "border-2"             : submit,
@@ -22,15 +25,16 @@ export function Tabs(props) {
     const [activeIndex, setActiveIndex] = createSignal(0);
     const keys = () => props.children.map(c => c.key);
     const values = () => props.children.map(c => c.value);
-    
+
     return <>
         <div class="flex mb-12">
             <For each={keys()}>
                 {(item, index) => <span
-                        onClick={() => setActiveIndex(index())}
-                        classList={classList(false)}>{item}</span>}
+                    onClick={() => setActiveIndex(index())}
+                    classList={classList(activeIndex() === index(), false)}>
+                    {item}</span>}
             </For>
-            <button classList={classList(true)} onClick={props.onSubmit}>
+            <button classList={classList(false, true)} onClick={props.onSubmit}>
                 {props.submit}
             </button>
         </div>

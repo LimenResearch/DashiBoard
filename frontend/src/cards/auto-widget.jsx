b7d176c02fd64adaa8dd656170fd0c4c470fb308
piever <pietro.vertechi@protonmail.com>
2025-01-22 17:47:24 +0100

    represent missing required fields

--- frontend/src/cards/auto-widget.jsx
+++ frontend/src/cards/auto-widget.jsx
@@ -28,40 +28,49 @@ export function AutoWidget(props) {
     const updateValue = x => setter(props.key, parseAll(x));
     const visible = () => applyFilter(props.visible, value());
     const required = () => applyFilter(props.required, value());
-    const label = () => required() ? props.label + " *" : props.label
+    const valid = () => {
+        const val = value()[props.key];
+        if (props.multiple) {
+            return val.length > 0;
+        } else if (props.type === "text") {
+            return !!val;
+        } else {
+            return val != null;
+        }
+    }
+
+    const starClassList = () => ({
+        "invisible": !required(),
+        "text-red-800": !valid(),
+        "text-blue-800": valid(),
+    });
+
+    const label = <label for={props.id + props.key} class={selectClass}>
+        <span classList={starClassList()}>* </span><span>{props.label}</span>
+    </label>
 
     let wdg;
     switch (props.widget) {
         case "select":
             const options = () => applyTemplate(props.options, defaults());
             const wdgProps = createOptions(options);
-            wdg = <>
-                <label for={props.id + props.key} class={selectClass}>
-                    {label()}
-                </label>
-                <Select id={props.id + props.key} onChange={updateValue}
-                    multiple={props.multiple} class="mb-2" {...wdgProps}
-                    placeholder={props.placeholder} initialValue={init[props.key] || ""}
-                    required={required()}>
-                </Select>
-            </>;
+            wdg = <Select id={props.id + props.key} onChange={updateValue}
+                multiple={props.multiple} class="mb-2" {...wdgProps}
+                placeholder={props.placeholder} initialValue={init[props.key] || ""}
+                required={required()}>
+            </Select>;
             break;
         case "input":
-            wdg = <>
-                <label for={props.id + props.key} class={selectClass}>
-                    {label()}
-                </label>
-                <Input id={props.id + props.key}
-                    onChange={ev => updateValue(ev.target.value)}
-                    class="w-full mb-2" type={props.type}
-                    value={init[props.key]} placeholder={props.placeholder}
-                    min={props.min} max={props.max} step={props.step}
-                    required={required()}>
-                </Input>
-            </>;
+            wdg = <Input id={props.id + props.key}
+                onChange={ev => updateValue(ev.target.value)}
+                class="w-full mb-2" type={props.type}
+                value={init[props.key]} placeholder={props.placeholder}
+                min={props.min} max={props.max} step={props.step}
+                required={required()}>
+            </Input>;
             break;
         default:
             console.log("widget not available");
     }
-    return <Show when={visible()}>{wdg}</Show>;
+    return <Show when={visible()}>{label}{wdg}</Show>;
 }

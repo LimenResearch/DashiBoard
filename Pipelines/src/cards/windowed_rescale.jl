edf73ee65d01f71ebe95adcba9ffee1993da11c7
DarioSarra <dariosarra@gmail.com>
2025-01-29 13:07:49 +0000

    using internal indexing

--- Pipelines/src/cards/windowed_rescale.jl
+++ Pipelines/src/cards/windowed_rescale.jl
@@ -25,8 +25,9 @@ Notes:
     window_size::Union{Int, String}
     sorters::Vector{String} = String[]
     by::Vector{String} = String[]
-    suffix::String = "rescaled"
-    function WindowedRescaleCard(method, columns, window_type, window_size, sorters=nothing, by=String[], suffix="rescaled")
+    suffix::String = "windowed"
+    idx_uid::String = string(uuid4())
+    function WindowedRescaleCard(method, columns, window_type, window_size, sorters = nothing, by = String[], suffix = "windowed", idx_uid = string(uuid4()))
         # Validate method
         !(method in keys(RESCALERS)) && throw(ArgumentError("Invalid `method`. Supported methods are: zscore, maxabs, minmax."))
         isempty(RESCALERS[method].stats) && throw(ArgumentError("Method `$method` is not supported for windowing."))
@@ -34,69 +35,82 @@ Notes:
         !(window_type in ["index", "time"]) && throw(ArgumentError("Invalid `window_type`. Must be 'index' or 'time'."))
         window_type == "time" && ismissing(sorters) && throw(ArgumentError("`sorters` must be provided for `window_type='time'."))
         window_type == "index" && !(window_size isa Int) && throw(ArgumentError("`window_size` must be an integer for `window_type='index'."))
-        new(method, columns, window_type, window_size, sorters, by, suffix)
+        new(method, columns, window_type, window_size, sorters, by, suffix, idx_uid)
     end
 end
 
-function Pipelines.train(
-    repo::Repository,
-    card::WindowedRescaleCard,
-    source::AbstractString;
-    schema=nothing
+function init_source(source, sorters, by, idx_uid)
+    return From(source) |>
+        Partition(
+        order_by = Get.(sorters),
+        by = Get.(by)
+    ) |>
+        Define(idx_uid => Agg.row_number())
+end
+
+function partition_source(idx_uid, by, window_size)
+    return Partition(
+        ; order_by = [Symbol(idx_uid)],
+        by = getindex.(Get, by),
+        frame = (mode = :range, start = -window_size, finish = -1)
     )
-    (; columns, method, window_size, sorters, by) = card
+end
+
+function Pipelines.train(
+        repo::Repository,
+        card::WindowedRescaleCard,
+        source::AbstractString;
+        schema = nothing
+    )
+    (; columns, method, window_size, sorters, by, idx_uid) = card
     (; stats) = RESCALERS[method]
 
-    
+
     # Create `_indices` column and compute aggregates
     aggregate_selects = [
         Symbol(col, "_", stat_name) => stat_function(Get(Symbol(col)))
-        for col in columns for (stat_name, stat_function) in stats
+            for col in columns for (stat_name, stat_function) in stats
     ]
 
-    q = From(source) |>
-        Partition(
-            ; order_by=Get.(Symbol.(sorters)),
-            by=getindex.(Get, by),
-            frame=(mode=:range, start=-window_size, finish=-1)
-        ) |>
+    init_query = init_source(source, sorters, by, idx_uid)
+    partition_query = partition_source(idx_uid, by, window_size)
+    q = init_query |>
+        partition_query |> 
         Select(
-            :_indices => Agg.row_number(),  # Include `_indices` for alignment
+            Symbol(idx_uid),
             getindex.(Get, by)...,  # Include grouping keys
             aggregate_selects...  # Compute aggregates
         ) |>
-        Where(Fun(">", Get._indices, window_size))
+        Where(Fun(">", Get(Symbol(idx_uid)), window_size))
 
-    return DBInterface.execute(fromtable, repo, q; schema=schema)
+    return DBInterface.execute(fromtable, repo, q; schema = schema)
 end
 
 function Pipelines.evaluate(
-    repo::Repository,
-    card::WindowedRescaleCard,
-    stats_tbl::SimpleTable,
-    (source, target)::Pair;
-    schema=nothing
-)
-    (; columns, method, suffix, sorters, window_size, by) = card
+        repo::Repository,
+        card::WindowedRescaleCard,
+        stats_tbl::SimpleTable,
+        (source, target)::Pair;
+        schema = nothing
+    )
+    (; columns, method, suffix, sorters, window_size, by, idx_uid) = card
     (; transform) = RESCALERS[method]
 
     available_columns = colnames(repo, source; schema)
     transformed = [string(c, '_', suffix) => transform(c) for c in columns if c in available_columns]
     target_columns = union(available_columns, first.(transformed))
-    join_cols = union(by, ["_indices"])
+    join_cols = union(by, [idx_uid])
     eqs = (.==).(Get.(join_cols), GetStats.(join_cols))
+    init_query = init_source(source, sorters, by, idx_uid)
+    partition_query = partition_source(idx_uid, by, window_size)
 
-    with_table(repo, stats_tbl; schema) do tbl_name
-        query = From(source) |>
-            Partition(
-                ; order_by=Get.(Symbol.(sorters)),
-                by=getindex.(Get, by),
-                frame=(mode=:range, start=-window_size, finish=-1)
-            ) |>
+    return with_table(repo, stats_tbl; schema) do tbl_name
+        q = init_query |>
+            partition_query |>
             Define(:_indices => Agg.row_number()) |>  # Regenerate `_indices` to join
             Join("stats" => From(tbl_name), on = Fun.and(eqs...)) |>
             Define(transformed...) |> # Apply transformations using stats
             Select(Get.(target_columns)...)
-        replace_table(repo, query, target; schema)
+        replace_table(repo, q, target; schema)
     end
 end

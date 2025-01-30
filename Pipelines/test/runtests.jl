de6244985eb5b000847e00b97c31a6184a5f67af
piever <pietro.vertechi@protonmail.com>
2025-01-30 17:58:12 +0100

    wip

--- Pipelines/test/runtests.jl
+++ Pipelines/test/runtests.jl
@@ -4,33 +4,7 @@ using OrderedCollections, EasyConfig, Dates, Distributions
 using FunSQL: Get
 using Test
 
-@testset "evaluation order" begin
-    struct TrivialCard <: Pipelines.AbstractCard
-        inputs::Vector{String}
-        outputs::Vector{String}
-    end
-    Pipelines.inputs(t::TrivialCard) = OrderedSet(t.inputs)
-    Pipelines.outputs(t::TrivialCard) = OrderedSet(t.outputs)
-
-    nodes = [
-        Pipelines.Node(TrivialCard(["temp"], ["pred humid"]), true),
-        Pipelines.Node(TrivialCard(["pred humid"], ["pred wind"]), true),
-        Pipelines.Node(TrivialCard(["wind", "wind name"], ["pred temp"]), true),
-        Pipelines.Node(TrivialCard(["wind"], ["wind name"]), true),
-    ]
-
-    @test Pipelines.evaluation_order!(nodes) == [4, 3, 1, 2]
-
-    nodes = [
-        Pipelines.Node(TrivialCard(["temp"], ["pred humid"]), false),
-        Pipelines.Node(TrivialCard(["pred humid"], ["pred wind"]), true),
-        Pipelines.Node(TrivialCard(["wind", "wind name"], ["pred temp"]), false),
-        Pipelines.Node(TrivialCard(["wind"], ["wind name"]), true),
-    ]
-
-    @test Pipelines.evaluation_order!(nodes) == [4, 3, 2]
-end
-
+include("evaluation.jl")
 @testset "basic funnel" begin
     spec = open(JSON3.read, joinpath(@__DIR__, "static", "configs", "spec.json"))
     schema = "schm"

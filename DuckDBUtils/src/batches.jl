24991b34311851eccda1558bee367ffa0e6b55fc
Pietro Vertechi <pietro.vertechi@protonmail.com>
2025-03-12 16:48:05 +0100

    Fix batches for type unstable columns take 2 (#46)

--- DuckDBUtils/src/batches.jl
+++ DuckDBUtils/src/batches.jl
@@ -16,16 +16,12 @@ function _numobs(cols)
 end
 
 """
-    _init(cols)
+    _init(schm::Tables.Schema)
 
-Initialize an empty table with the same schema as the column-based table `cols`.
+Initialize an empty table with schema `schm`.
 """
-function _init(cols)
-    assert_columnaccess(cols)
-    ns = Tables.columnnames(cols)
-    return OrderedDict{Symbol, AbstractVector}(
-        n => similar(Tables.getcolumn(cols, n), 0) for n in ns
-    )
+function _init(schm::Tables.Schema)
+    return OrderedDict{Symbol, Vector}(n => T[] for (n, T) in zip(schm.names, schm.types))
 end
 
 """
@@ -44,27 +40,36 @@ end
 
 # Stream DuckDB data
 
-"""
-    struct Batches{T}
-        chunks::T
-        batchsize::Int
-        nrows::Int
-    end
-
-Let `chunks` be a partitioned table with `nrows` in total.
-Then, return an iterator of column-based tables with `batchsize` rows each.
-
-!!! note
-    `chunks` can in general be obtained as the output of `Tables.partitions`.
-"""
-struct Batches{T}
+struct Batches{T, names, types}
     chunks::T
+    schema::Tables.Schema{names, types}
     batchsize::Int
     nrows::Int
+
+    function Batches(
+            chunks::T,
+            schema::Tables.Schema{names, types},
+            batchsize::Integer,
+            nrows::Integer
+        ) where {T, names, types}
+
+        return new{T, names, types}(chunks, schema, batchsize, nrows)
+    end
+end
+
+"""
+    Batches(tbl, batchsize::Integer, nrows::Integer)
+
+Construct a `Batches` iterator based on a table `tbl` with `nrows` in total.
+The resulting object iterates column-based tables with `batchsize` rows each.
+"""
+function Batches(tbl, batchsize::Integer, nrows::Integer)
+    chunks, schema = Tables.partitions(tbl), Tables.schema(tbl)
+    return Batches(chunks, schema, batchsize, nrows)
 end
 
 function Base.eltype(::Type{Batches{T}}) where {T}
-    return OrderedDict{Symbol, AbstractVector}
+    return OrderedDict{Symbol, Vector}
 end
 
 Base.length(r::Batches) = cld(r.nrows, r.batchsize)
@@ -74,8 +79,8 @@ Base.size(r::Batches) = (length(r),)
 function Base.iterate(r::Batches, (res, j) = (iterate(r.chunks), 0))
     isnothing(res) && return nothing
     chunk, st = res
+    batch = _init(r.schema)
     cols = Tables.columns(chunk)
-    batch = _init(cols)
     while _numobs(batch) < r.batchsize
         if _numobs(cols) ≤ j
             res, j = iterate(r.chunks, st), 0

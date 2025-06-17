a8fb56ecc9420f7cdd8bc8f550482cfa46c460a2
piever <pietro.vertechi@protonmail.com>
2025-06-17 11:09:44 +0200

    additional tests

--- Pipelines/test/utils.jl
+++ Pipelines/test/utils.jl
@@ -7,4 +7,9 @@
     @test c == "a_2"
     c = Pipelines.new_name("a", ["b"])
     @test c == "a_1"
+
+    @test Pipelines.to_string_dict((a = 1, b = 2)) == Dict("a" => 1, "b" => 2)
+    @test Pipelines.to_string_dict(1 + 2im) == Dict("re" => 1, "im" => 2)
+
+    @test Pipelines.to_symbol_dict(Dict("a" => 1, "b" => 2)) == Dict(:a => 1, :b => 2)
 end

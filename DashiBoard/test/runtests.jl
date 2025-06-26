69bbd0429bfc734622bddd3a57b1f8a821e5be0c
piever <pietro.vertechi@protonmail.com>
2025-06-26 12:24:53 +0200

    fix more paths

--- DashiBoard/test/runtests.jl
+++ DashiBoard/test/runtests.jl
@@ -1,6 +1,6 @@
 include("dashiboard.jl")
 
-include("../DuckDBUtils/test/runtests.jl")
-include("../DataIngestion/test/runtests.jl")
-include("../Pipelines/test/runtests.jl")
-include("../StreamlinerCore/test/runtests.jl")
+include("../../DuckDBUtils/test/runtests.jl")
+include("../../DataIngestion/test/runtests.jl")
+include("../../Pipelines/test/runtests.jl")
+include("../../StreamlinerCore/test/runtests.jl")

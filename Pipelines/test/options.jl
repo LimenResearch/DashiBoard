786f9148851e7dff2593f96b45d066b8d19e8ea9
piever <pietro.vertechi@protonmail.com>
2025-07-28 10:30:26 +0200

    options testing

--- Pipelines/test/options.jl
+++ Pipelines/test/options.jl
@@ -1,11 +1,18 @@
 @testset "options" begin
     d = JSON.parsefile(joinpath(@__DIR__, "static", "configs", "options.json"))
-    @test Pipelines.extract_options(d, "method", "dbscan") == Dict(
+    c = d["separated"]
+    @test Pipelines.extract_options(c, "method", "dbscan") == Dict(
         "radius" => 1.2,
         "min_neighbors" => 5,
         "min_cluster_size" => 12
     )
-    @test Pipelines.extract_options(d, "method", "kmeans") == Dict(
+    @test Pipelines.extract_options(c, "method", "kmeans") == Dict(
         "n_classes" => 7
+    )
+
+    c = d["joint"]
+
+    @test Pipelines.extract_options(c, "method", "kmeans") == Dict(
+        "n_classes" => 12
     )
 end

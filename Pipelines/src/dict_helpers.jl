1b4dfaa221c00b5e47856f20716e858dd892f58f
piever <pietro.vertechi@protonmail.com>
2025-11-20 12:44:07 +0100

    single pass

--- Pipelines/src/dict_helpers.jl
+++ Pipelines/src/dict_helpers.jl
@@ -3,61 +3,66 @@
 abstract type AbstractDictHelper end
 
 function _keys end
-function splice end
 
 isinstance(v, h::AbstractDictHelper) = v isa AbstractDict && issetequal(keys(v), _keys(h))
 
-function _apply_helper(h::AbstractDictHelper, x, params::AbstractDict)
-    return x isa Union{AbstractDict, AbstractVector} ? apply_helper(h, x, params) : x
+struct SplicedValues
+    vals::Vector{Any}
 end
 
-function apply_helper(h::AbstractDictHelper, d::AbstractDict, params::AbstractDict)
+isspliced(x) = x isa SplicedValues
+
+function _apply_helpers_nested(hs, x, params)
+    x1 = _apply_helpers(hs, x, params)
+    for h in hs
+        isinstance(x1, h) && return _apply_helpers(hs, h(x1, params), params)
+    end
+    return x1
+end
+
+_apply_helpers(hs, x, params::AbstractDict) = x
+
+function _apply_helpers(hs, x::SplicedValues, params::AbstractDict)
+    vals = _apply_helpers(hs, x.vals, params)
+    return SplicedValues(vals)
+end
+
+function _apply_helpers(hs, d::AbstractDict, params::AbstractDict)
     d1 = Dict{String, Any}()
     for (k, x) in pairs(d)
-        d1[k] = if isinstance(x, h)
-            splice(h) && throw(ArgumentError("Can only splice inside a list"))
-            h(x, params)
-        else
-            _apply_helper(h, x, params)
-        end
+        val = _apply_helpers_nested(hs, x, params)
+        isspliced(val) ? throw(ArgumentError("Can only splice inside a list")) : (d1[k] = val)
     end
     return d1
 end
 
-function apply_helper(h::AbstractDictHelper, v::AbstractVector, params::AbstractDict)
+function _apply_helpers(hs, v::AbstractVector, params::AbstractDict)
     v1 = Any[]
     for x in v
-        if isinstance(x, h)
-            val = h(x, params)
-            splice(h) ? append!(v1, val) : push!(v1, val)
-        else
-            push!(v1, _apply_helper(h, x, params))
-        end
+        val = _apply_helpers_nested(hs, x, params)
+        isspliced(val) ? append!(v1, val.vals) : push!(v1, val)
     end
     return v1
 end
 
-apply_helpers(hs, d::AbstractDict, ps) = foldl((dn, h) -> apply_helper(h, dn, ps), hs, init = d)
+apply_helpers(hs, d::AbstractDict, params::AbstractDict) = _apply_helpers(hs, d, params)
 
 ## Instances
 
 struct VariableHelper <: AbstractDictHelper end
 
 _keys(::VariableHelper) = Set(["-v"])
-splice(::VariableHelper) = false
 
 (::VariableHelper)(v, params) = params[v["-v"]]
 
 struct SpliceHelper <: AbstractDictHelper end
 
 _keys(::SpliceHelper) = Set(["-s"])
-splice(::SpliceHelper) = true
 
-(::SpliceHelper)(v, params) = params[v["-s"]]
+(::SpliceHelper)(v, params) = SplicedValues(params[v["-s"]])
 
 struct NumberedHelper <: AbstractDictHelper end
 
 _keys(::NumberedHelper) = Set(["-c", "-n"])
-splice(::NumberedHelper) = true
 
-(::NumberedHelper)(v, _) = string.(v["-c"], "_", 1:v["-n"])
+(::NumberedHelper)(v, _) = SplicedValues(string.(v["-c"], "_", 1:v["-n"]))

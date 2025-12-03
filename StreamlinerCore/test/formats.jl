4147323b907c136931f4b3234862d593af50c068
piever <pietro.vertechi@protonmail.com>
2025-12-03 12:28:18 +0100

    fix tests

--- StreamlinerCore/test/formats.jl
+++ StreamlinerCore/test/formats.jl
@@ -2,7 +2,7 @@
     fmt0 = StreamlinerCore.FlatFormat()
     fmt1 = StreamlinerCore.SpatialFormat{1}()
     fmt2 = StreamlinerCore.SpatialFormat{2}()
-    @test Shape(5).format === fmt0
+    @test Shape((), 5).format === fmt0
     @test Shape((1,), 2).format === fmt1
     @test Shape((1, 2), 3).format === fmt2
 
@@ -15,13 +15,13 @@
     x = rand(110, 16)
     @test StreamlinerCore.unflatten(x, Shape((2, 5), 11)) == reshape(x, 2, 5, 11, 16)
 
-    @test StreamlinerCore.instantiate(formatter, Shape(110), Shape(fmt2)) === (
+    @test StreamlinerCore.instantiate(formatter, Shape((), 110), Shape(fmt2)) === (
         Base.Fix2(StreamlinerCore.unflatten, Shape((2, 5), 11)),
         Shape((2, 5), 11),
     )
 
     @test StreamlinerCore.instantiate(formatter, Shape((2, 5), 11), Shape(fmt0)) === (
         MLUtils.flatten,
-        Shape(110),
+        Shape((), 110),
     )
 end

08ff173726d4500605bbe58fcc6a237dfd423949
piever <pietro.vertechi@protonmail.com>
2026-08-07 21:45:10 +0200

    restore request types

--- DashiBoard/src/launch.jl
+++ DashiBoard/src/launch.jl
@@ -1,8 +1,8 @@
 function launch(
         data_directory::AbstractString;
-        host = "127.0.0.1",
-        port = 8080,
-        async = false,
+        host::AbstractString = "127.0.0.1",
+        port::Integer = 8080,
+        async::Bool = false,
         training_directory::AbstractString,
         model_directory::AbstractString,
         parser::Pipelines.Parser = Pipelines.default_parser()
@@ -10,22 +10,23 @@ function launch(
 
     router = HTTP.Router(
         HTTP.streamhandler(cors404),
-        HTTP.streamhandler(cors405),
-        CorsMiddleware
+        HTTP.streamhandler(cors405)
     )
 
-    HTTP.register!(router, "/get-acceptable-paths", HTTP.streamhandler(get_acceptable_paths))
-    HTTP.register!(router, "/load-files", HTTP.streamhandler(load_files))
-    HTTP.register!(router, "/get-card-widgets", HTTP.streamhandler(get_card_widgets))
-    HTTP.register!(router, "/evaluate-pipeline", HTTP.streamhandler(evaluate_pipeline))
-    HTTP.register!(router, "/fetch-data", fetch_data)
-    HTTP.register!(router, "/get-processed-data", get_processed_data)
+    HTTP.register!(router, "POST", "/get-acceptable-paths", HTTP.streamhandler(get_acceptable_paths))
+    HTTP.register!(router, "POST", "/load-files", HTTP.streamhandler(load_files))
+    HTTP.register!(router, "POST", "/get-card-widgets", HTTP.streamhandler(get_card_widgets))
+    HTTP.register!(router, "POST", "/evaluate-pipeline", HTTP.streamhandler(evaluate_pipeline))
+    HTTP.register!(router, "POST", "/fetch-data", fetch_data)
+    HTTP.register!(router, "GET", "/get-processed-data", get_processed_data)
+
+    cors_router = router |> CorsMiddleware
 
     return @with(
         Pipelines.PARSER => parser,
         Pipelines.MODEL_DIR => model_directory,
         Pipelines.TRAINING_DIR => training_directory,
         DataIngestion.DATA_DIR => data_directory,
-        async ? HTTP.listen!(router, host, port) : HTTP.listen(router, host, port)
+        async ? HTTP.listen!(cors_router, host, port) : HTTP.listen(cors_router, host, port)
     )
 end
